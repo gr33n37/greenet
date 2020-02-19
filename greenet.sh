a36f933698b029db04815ea04d205b66ebbbaf7a
josh green <bcsjoshgreen25@gmail.com>
2020-02-19 16:49:28 +0300

    Create greenet.sh

---
+++ greenet.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+clear
+echo -e "\e[1;31m
+   ____ ____  __________ _   _ __________ 
+  / ___|  _ \|___ /___ /| \ | |___ /___  |
+ | |  _| |_) | |_ \ |_ \|  \| | |_ \  / / 
+ | |_| |  _ < ___) |__) | |\  |___) |/ /  
+  \____|_| \_\____/____/|_| \_|____//_/   
+  ========= G R E E N E T =========                    \e[0m "
+
+echo -e "\n\tmenu"
+echo -e "\e[1;32m
+[1] Information Gathering
+[2] Password Attacks
+[3] Wireless Testing
+[4] Exploitation Tools
+[5] Sniffing & Spoofing
+[5] Web Hacking
+[6] Private Web Hacking
+[7] Post Exploitation
+[0] INSTALL & UPDATE\e[0m"
+
+echo -en "\nGREENET:~# "
+#read -p "GREENET:~# "
+read menu
+
+if [ $menu == "stacey" ]; then
+
+function test()
+{
+echo -e "\nthe name of bf is josh" 
+}
+
+test
+else 
+echo "shit"
+fi
+
+
+: 'class informationGatheringMenu:
+    menuLogo = '''
+    88 88b 88 888888  dP"Yb
+    88 88Yb88 88__   dP   Yb
+    88 88 Y88 88""   Yb   dP
+    88 88  Y8 88      YbodP
+    '''
+
+    def __init__(self):
+        clearScr()
+        print(self.menuLogo)
+
+        print("  {1}--Nmap - Network Mapper")
+        print("  {2}--Setoolkit")
+        print("  {3}--Host To IP")
+        print("  {4}--WPScan")
+        print("  {5}--CMSmap")
+        print("  {6}--XSStrike")
+        print("  {7}--Doork")
+        print("  {8}--Crips\n  ")
+        print("  {99}-Back To Main Menu \n")
+        choice2 = raw_input(fsocietyPrompt)'
+
+
+
+
+
+: 'HEADER = '\033[95m'
+IMPORTANT = '\33[35m'
+NOTICE = '\033[33m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+RED = '\033[91m'
+END = '\033[0m'
+UNDERLINE = '\033[4m'
+LOGGING = '\33[34m''
